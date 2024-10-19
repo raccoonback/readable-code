08891360eb2ba1b2df4c64adc299fe2764ca06ec
oliver.ko <oliver.ko@kakaomobility.com>
2024-10-19 16:33:30 +0900

    기능 유지보수하기 (2) - 알고리즘 교체하기

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -2,6 +2,7 @@ package cleancode.minesweeper.tobe;
 
 import cleancode.minesweeper.tobe.minesweeper.MineSweeper;
 import cleancode.minesweeper.tobe.minesweeper.config.GameConfig;
+import cleancode.minesweeper.tobe.minesweeper.gamelevel.Advanced;
 import cleancode.minesweeper.tobe.minesweeper.gamelevel.Middle;
 import cleancode.minesweeper.tobe.minesweeper.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.minesweeper.io.ConsoleOutputHandler;
@@ -10,7 +11,7 @@ public class GameApplication {
 
     public static void main(String[] args) {
         GameConfig gameConfig = new GameConfig(
-                new Middle(),
+                new Advanced(),
                 new ConsoleInputHandler(),
                 new ConsoleOutputHandler()
         );
